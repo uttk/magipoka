0a984a493e659cf6442d0ac168c7e49d7d2a274d
uttk <46495635+uttk@users.noreply.github.com>
2022-02-12 02:06:28 +0900

    feat: add utils

---
+++ src/helpers/utils.test.ts
@@ -0,0 +1,56 @@
+import fs from "fs/promises";
+import { vi, describe, it, expect } from "vitest";
+
+import { getExistsPath, getPageFilePaths, isSameExtension } from "./utils";
+
+const spy = vi.spyOn(fs, "access");
+
+describe("getExistsPath() Tests", () => {
+  it("return a exists path", async () => {
+    const paths = ["/bad/path", "/good/path"];
+
+    const mock = spy.mockImplementation(async (path) => {
+      if (path !== paths[1]) throw new Error();
+    });
+
+    const result = await getExistsPath(paths);
+
+    expect(result).toBe(paths[1]);
+    expect(mock).toBeCalled();
+  });
+});
+
+describe("isSameExtension() Tests", () => {
+  it("return true when match passed extensions", () => {
+    const exts = [".js", ".ts"];
+
+    const matchFiles = ["index.js", "hoge.ts"];
+    matchFiles.forEach((file) =>
+      expect(isSameExtension(file, exts)).toBe(true)
+    );
+
+    const notMatchFiles = ["hello.jsx", "world.tsx"];
+    notMatchFiles.forEach((file) =>
+      expect(isSameExtension(file, exts)).toBe(false)
+    );
+  });
+});
+
+const readdirSpy = vi.spyOn(fs, "readdir");
+
+describe("getPageFilePaths() Tests", () => {
+  it("return file paths from passed root path", async () => {
+    const exts = [".page"];
+
+    const mockFiles = [
+      { name: "hoge.page", isFile: () => true, isDirectory: () => false },
+      { name: "hoge.page", isFile: () => false, isDirectory: () => false },
+    ] as any;
+
+    readdirSpy.mockReturnValue(mockFiles);
+
+    const result = await getPageFilePaths("/root", exts);
+
+    expect(result).toStrictEqual([`/root/${mockFiles[0].name}`]);
+  });
+});
---
+++ src/helpers/utils.ts
@@ -0,0 +1,39 @@
+import path from "path";
+import fs from "fs/promises";
+
+export const getExistsPath = (paths: string | string[]): Promise<string> => {
+  paths = Array.isArray(paths) ? paths : [paths];
+  return Promise.any<string>(paths.map((v) => fs.access(v).then(() => v)));
+};
+
+export const isSameExtension = (name: string, exts: string[]): boolean => {
+  return !!name.match(new RegExp(`(?:${exts.join("|").replace(".", "\\.")})$`));
+};
+
+export const getPageFilePaths = async (
+  root: string,
+  pageExtensions: string[]
+): Promise<string[]> => {
+  const filePaths: string[] = [];
+  const tasks: Promise<string[]>[] = [];
+
+  const dirents = await fs.readdir(root, { withFileTypes: true });
+
+  for (const child of dirents) {
+    const folderPath = path.join(root, child.name);
+
+    if (child.isFile()) {
+      if (isSameExtension(child.name, pageExtensions)) {
+        filePaths.push(folderPath);
+      }
+    } else if (child.isDirectory()) {
+      tasks.push(getPageFilePaths(folderPath, pageExtensions));
+    }
+  }
+
+  const childPaths = await Promise.all(tasks);
+
+  filePaths.push(...childPaths.flat());
+
+  return filePaths;
+};
